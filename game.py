25d563e1fe34569f32d262651cf4a65dae389c4e
CZnyu <CZnyu@users.noreply.github.com>
2020-06-06 18:33:23 -0400

    Determining the Winner and Results

--- game.py
+++ game.py
@@ -1,17 +1,62 @@
 print("Rock, Paper, Scissors, Shoot!")
 
+import random
+
+arr = ["Rock","Paper","Scissors"]
+
 def options (s):
+    if s == "Rock":
+        return  arr[0]
     if s == "rock":
-        return  "Rock"
+        return  arr[0]
+    elif s == "Paper":
+        return arr[1]
     elif s == "paper":
-        return "Paper"
+        return arr[1]
+    elif s == "Scissors":
+        return arr[2]
     elif s == "scissors":
-        return "Scissors"
+        return arr[2]
     else:
         return ("Hmmm...that's not an option. Try entering Rock, Paper, or Scissors", quit)
 
-print("--------------------")
-selection = input("Enter Rock, Paper, or Scissors Here:")
-item = options(selection)
-print("You choose:", item)
 
+
+print("--------------------")
+user_choice = input("Enter Rock, Paper, or Scissors Here: ")
+item = options(user_choice)
+print("YOU CHOSE:", item)
+
+computer_choice = random.choice(arr)
+print(f"COMPUTER CHOSE: '{computer_choice}'")
+
+outcomes = {
+    arr[0]:{
+        arr[0]: None,
+        arr[1]: arr[1],
+        arr[2]: arr[0],
+    },
+    arr[1]:{
+        arr[0]: arr[1],
+        arr[1]: None,
+        arr[2]: arr[2],
+    },   
+    arr[2]:{
+        arr[0]: arr[0],
+        arr[1]: arr[2],
+        arr[2]: None,
+    },   
+ }
+
+
+
+winning_choice = outcomes[user_choice][computer_choice]
+
+if winning_choice:
+    if winning_choice == user_choice:
+        print("YOU WON")
+    elif winning_choice == computer_choice:
+        print("YOU LOST")
+else:
+        print("TIE")
+print("Thanks for playing. Please play again!")
